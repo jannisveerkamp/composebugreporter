bf6a7d40969883f4fc0c3ff9e8658c26f0d94bc8
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2021-11-18 11:09:14 +0100

    Add example app to reproduce the bug

---
+++ app/src/main/java/de/jannisveerkamp/composebugreporter/ui/MainScreen.kt
@@ -0,0 +1,105 @@
+package de.jannisveerkamp.composebugreporter.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.HiltViewModelFactory
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.lifecycle.HiltViewModel
+import de.jannisveerkamp.composebugreporter.ui.theme.ComposeBugreporterTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "start") {
+        composable("start") { navBackStackEntry ->
+            val viewModel = viewModel(
+                FirstScreenViewModel::class.java,
+                factory = HiltViewModelFactory(LocalContext.current, navBackStackEntry)
+            )
+            val text = viewModel.data.collectAsState()
+
+            Content(
+                text = text.value,
+                buttonText = "Open Dialog",
+                onClick = {
+                    navController.navigate("dialog")
+                },
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        // replace this with composable("dialog") won't let the app crash
+        dialog("dialog") { navBackStackEntry ->
+            val viewModel = viewModel(
+                DialogViewModel::class.java,
+                factory = HiltViewModelFactory(LocalContext.current, navBackStackEntry)
+            )
+            val text = viewModel.data.collectAsState()
+
+            Content(
+                modifier = Modifier.padding(16.dp),
+                text = text.value,
+                buttonText = "Close Dialog",
+                onClick = {
+                    viewModel.data.tryEmit("123")
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun Content(text: String, buttonText: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("$text screen")
+        Button(onClick = onClick) {
+            Text(buttonText)
+        }
+    }
+}
+
+@HiltViewModel
+class FirstScreenViewModel @Inject constructor() : ViewModel() {
+    val data: StateFlow<String> = MutableStateFlow("first")
+}
+
+@HiltViewModel
+class DialogViewModel @Inject constructor() : ViewModel() {
+    val data: MutableStateFlow<String> = MutableStateFlow("dialog")
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeBugreporterTheme {
+        MainScreen()
+    }
+}
