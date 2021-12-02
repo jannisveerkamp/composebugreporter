5bdfba32abda8ff49212fcbf91c927cfde4af35c
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2021-12-02 20:33:43 +0100

    Add KeyboardScrollScreen

--- app/src/main/java/de/jannisveerkamp/composebugreporter/MainActivity.kt
+++ app/src/main/java/de/jannisveerkamp/composebugreporter/MainActivity.kt
@@ -5,12 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.ProvideWindowInsets
 import dagger.hilt.android.AndroidEntryPoint
+import de.jannisveerkamp.composebugreporter.ui.KeyboardScrollScreen
 import de.jannisveerkamp.composebugreporter.ui.MainScreen
 import de.jannisveerkamp.composebugreporter.ui.theme.ComposeBugreporterTheme
 
@@ -26,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     // A surface container using the 'background' color from the theme
                     Surface(color = MaterialTheme.colors.background) {
                         MainScreen()
+                        //KeyboardScrollScreen()
                     }
                 }
             }
