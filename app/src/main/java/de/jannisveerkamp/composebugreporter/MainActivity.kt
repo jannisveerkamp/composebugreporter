3958a3de7a47903be729247483b01b72ed18dfe5
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2022-11-09 10:11:11 +0100

    Add huge text

--- app/src/main/java/de/jannisveerkamp/composebugreporter/MainActivity.kt
+++ app/src/main/java/de/jannisveerkamp/composebugreporter/MainActivity.kt
@@ -3,15 +3,26 @@ package de.jannisveerkamp.composebugreporter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
-import de.jannisveerkamp.composebugreporter.ui.MainScreen
 import de.jannisveerkamp.composebugreporter.ui.theme.ComposeBugreporterTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val hugeText = "abcd-efgh-".repeat(50_000)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +30,23 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeBugreporterTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    MainScreen()
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.padding(top = 24.dp, bottom = 48.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        Text(text = "${hugeText.length}")
+                        LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+                        Text(
+                            text = hugeText,
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxWidth()
+                                .verticalScroll(rememberScrollState())
+                        )
+                    }
                 }
             }
         }
